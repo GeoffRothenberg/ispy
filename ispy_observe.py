7703dd91b625899898f6672e14b33d96b5605889
Geoffrey <rothenberg.g@husky.neu.edu>
2021-04-19 21:15:36 -0400

    combined watchdog handlers with the watchdog observer logic

--- ispy_observe.py
+++ ispy_observe.py
@@ -8,6 +8,33 @@ from watchdog.events import PatternMatchingEventHandler
 watchlist = "ispy_watchlist.txt" #name of watchlist file, change once agreed on a file name
 
 observelist = "ispy_directories.txt" #name of file containing the directory names to watch
+class Handlers(watchdog.events.PatternMatchingEventHandler):
+    def __init__(self): 
+         watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv'],
+                                                             ignore_directories=True, case_sensitive=False)
+         logfile = open("logfile.txt", "x")
+         logfile.close()
+
+    def on_Created(self, event):
+        logfile.open("logfile.txt", "a")
+        logfile.write("{0}- was created at {1}".format(events.src_path,  strftime(date.time.now())))
+        logfile.close()
+
+    def on_modified(self, event):
+        logfile.open("logfile.txt", "a")
+        logfile.write("{0}- was modified at {1}".format(events.src_path,  strftime(date.time.now())))
+
+        logfile.close()
+
+    def on_deleted(self, event):
+        logfile.open("logfile.txt", "a")
+        logfile.write("{0}- was deleted at {1}".format(events.src_path,  strftime(date.time.now())))
+        logfile.close()
+
+    def on_moved(self, event):
+        logfile.open("logfile.txt", "a")
+        logfile.write("{0}- was moved at {1}".format(events.src_path,  strftime(date.time.now())))
+        logfile.close()
 
 if __name__ == "__main__":
     #set configuration - change format? user? clientip? process? processname? filename/pathname? 
@@ -22,7 +49,7 @@ if __name__ == "__main__":
         files = [line.rstrip('\n') for line in filelist]
 
     #set event handler - do I need to change this to customized?
-    event_handler = PatternMatchingEventHandler(patterns=files)
+    event_handler = Handlers(PatternMatchingEventHandler(patterns=files))
 
     observer = Observer()
     
