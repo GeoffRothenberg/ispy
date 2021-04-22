6733a7febaf9a3753032c613307694435eb6c1bd
Geoffrey <rothenberg.g@husky.neu.edu>
2021-04-22 12:45:14 -0400

    added comments and getting uid of process runner

--- ispy_observe.py
+++ ispy_observe.py
@@ -1,6 +1,6 @@
 import sys
 import time
-
+import os
 
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
@@ -11,35 +11,41 @@ watchlist = "ispy_watchlist.txt" #name of watchlist file, change once agreed on 
 
 observelist = "ispy_directories.txt" #name of file containing the directory names to watch
 
+#Event Handler class
 class Handlers(FileSystemEventHandler):
+    #Initializing Handler object, creating the logfile
     def __init__(self): 
 
          logfile = open("ispy_logfile.txt", "a")
          logfile.close()
 
+    #Method to log a file creation event
     def on_created(self, event):
         logfile = open("ispy_logfile.txt", "a")
-        logfile.write("\n{0}- was created at {1}".format(event.src_path,  str(time.ctime(time.time()))))
+        logfile.write("\n{0}- was created at {1} by User:{2}".format(event.src_path,  str(time.ctime(time.time())), os.getuid()))
         logfile.close()
 
+    #Method to log a file modification event
     def on_modified(self, event):
         logfile = open("ispy_logfile.txt", "a")
-        logfile.write("\n{0}- was modified at {1}".format(event.src_path,  str(time.ctime(time.time()))))
+        logfile.write("\n{0}- was modified at {1} by User: {2}".format(event.src_path,  str(time.ctime(time.time())), os.getuid()))
         logfile.close()
       
 
+    #Method to log a file deletion event
     def on_deleted(self, event):
         logfile = open("ispy_logfile.txt", "a")
-        logfile.write("\n{0}- was deleted at {1}".format(event.src_path,  str(time.ctime(time.time()))))
+        logfile.write("\n{0}- was deleted at {1} by User: {2}".format(event.src_path,  str(time.ctime(time.time())), os.getuid()))
         logfile.close()
 
+    #Method to log a file relocation event
     def on_moved(self, event):
         logfile = open("ispy_logfile.txt", "a")
-        logfile.write("\n{0}- was moved at {1}".format(event.src_path,  str(time.ctime(time.time()))))
+        logfile.write("\n{0}- was moved at {1} by User: {2}".format(event.src_path,  str(time.ctime(time.time())), os.getuid()))
         logfile.close()
 
 if __name__ == "__main__":
-    #set configuration - change format? user? clientip? process? processname? filename/pathname? 
+     
 
     #path to directories containing the files to observe
     with open(observelist, "r") as directories:    
@@ -49,7 +55,7 @@ if __name__ == "__main__":
     with open(watchlist, "r") as filelist:
         files = [line.rstrip('\n') for line in filelist]
 
-    #set event handler - do I need to change this to customized?
+    #set event handler
    
     event_handler = Handlers()
 
